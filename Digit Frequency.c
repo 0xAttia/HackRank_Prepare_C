b6d474b6989891a52aab377784e0e2353e331369
Moustafa Attia <72319345+0xAttia@users.noreply.github.com>
2022-08-24 23:24:49 +0200

    Update Digit Frequency.c

--- Digit Frequency.c
+++ Digit Frequency.c
@@ -5,26 +5,22 @@
 
 int main() {
     
-    int i, j, count;
-    char c, *s = malloc(1000 * sizeof(char));
+    char *s = malloc(1000 * sizeof(char));
     
     //take input string
     scanf("%[^\n]", s);
     s = realloc(s, strlen(s) + 1);
         
     //calculate the frequency for each digit from 0 to 9    
-    for(i = 0; i <= 9; i++)
+    for(int i = 0; i <= 9; i++)
     {
-        count=0;
+        int count = 0;
         
         //search accross all string
-        for( j = 0; j < strlen(s); j++)
+        for(int j = 0; j < strlen(s); j++)
         {
-            //pick a single char
-            c = *(s+j);
-            
             //if match increment
-            if( c >= '0' && c<='9' && atoi(&c) == i)
+            if( s[j] == i+ '0')
                 count++;
         }
         printf("%d ", count);
