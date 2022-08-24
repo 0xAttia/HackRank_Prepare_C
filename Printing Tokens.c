10e545ee28a1e7af42c6da7cd98183a28dc394d5
Moustafa Attia <72319345+0xAttia@users.noreply.github.com>
2022-08-24 20:07:42 +0200

    Create Printing Tokens.c

---
+++ Printing Tokens.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    
+    //Write your logic to print the tokens of the sentence here.
+    while(*s)
+    {
+        *s != ' '? printf("%c",*s) : printf("\n");
+        s++;
+    }
+    return 0;
+}
